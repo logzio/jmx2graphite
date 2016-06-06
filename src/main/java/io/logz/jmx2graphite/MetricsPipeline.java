ff6e781f28fa96e9875552cfa360087fa1ef2508
Roi Rav-Hon <roi@logz.io>
2016-06-06 13:50:03 +0300

    One file left behind

--- src/main/java/io/logz/jmx2graphite/MetricsPipeline.java
+++ src/main/java/io/logz/jmx2graphite/MetricsPipeline.java
@@ -19,109 +19,44 @@ import java.util.stream.Collectors;
  * @author amesika
  */
 @DisallowConcurrentExecution
-public class MetricsPipeline implements Job {
+public class MetricsPipeline {
     private static final Logger logger = LoggerFactory.getLogger(MetricsPipeline.class);
 
-    private String jolokiaUrl;
-    private String graphiteHostname;
-    private int graphitePort;
-    private String serviceName;
-    private String serviceHost;
-    private int pollingIntervalSeconds;
-
-    private JolokiaClient client;
     private GraphiteClient graphiteClient;
+    private MetricsPoller poller;
 
-    @Inject
-    public MetricsPipeline(Jmx2GraphiteConfiguration conf) {
-        this(conf.jolokiaUrl, conf.graphiteHostname, conf.graphitePort, conf.serviceName,
-                conf.serviceHost,
-                conf.getGraphiteConnectTimeout(),
-                conf.graphiteSocketTimeout,
-                conf.intervalInSeconds,
-                conf.getGraphiteWriteTimeoutMs());
+    public MetricsPipeline(Jmx2GraphiteConfiguration conf, MetricsPoller poller) {
+
+        this.poller = poller;
+        this.graphiteClient = new GraphiteClient(conf.serviceHost, conf.serviceName, conf.graphiteHostname,
+                                                 conf.graphitePort, conf.getGraphiteConnectTimeout(),
+                                                 conf.graphiteSocketTimeout, conf.getGraphiteWriteTimeoutMs());
     }
 
-    public MetricsPipeline(String jolokiaUrl, String graphiteHostname, int graphitePort, String serviceName,
-                           String serviceHost, int graphiteConnectTimeout, int graphiteSocketTimeout,
-                           int pollingIntervalSeconds, int graphiteWriteTimeoutMs) {
-        this.jolokiaUrl = jolokiaUrl;
-        this.graphiteHostname = graphiteHostname;
-        this.graphitePort = graphitePort;
-        this.serviceName = serviceName;
-        this.serviceHost = serviceHost;
-        this.pollingIntervalSeconds = pollingIntervalSeconds;
-        client = createJolokiaClient();
-        graphiteClient = new GraphiteClient(serviceHost, serviceName, graphiteHostname, graphitePort,
-                graphiteConnectTimeout, graphiteSocketTimeout, graphiteWriteTimeoutMs);
-    }
+    public void pollAndSend()  {
 
-    public void poll()  {
+        List<MetricValue> metrics = poller.poll();
+
         try {
-            long pollingWindowStartSeconds = getPollingWindowStartSeconds();
             Stopwatch sw = Stopwatch.createStarted();
-            List<MetricBean> beans = client.getBeans();
-            logger.info("Found {} metric beans. Time = {}ms, for {}", beans.size(),
-                    sw.stop().elapsed(TimeUnit.MILLISECONDS),
-                    new Date(TimeUnit.SECONDS.toMillis(pollingWindowStartSeconds)));
-
-            sw.reset().start();
-            List<MetricValue> metrics = client.getMetrics(beans);
-            logger.info("metrics fetched. Time: {} ms; Metrics: {}", sw.stop().elapsed(TimeUnit.MILLISECONDS), metrics.size());
-            if (logger.isTraceEnabled()) printToFile(metrics);
-            metrics = changeTimeTo(pollingWindowStartSeconds, metrics);
-
             sw.reset().start();
             sendToGraphite(metrics);
             logger.info("metrics sent to Graphite. Time: {} ms, Failed metrics: {}",
                     sw.stop().elapsed(TimeUnit.MILLISECONDS),
                     graphiteClient.getFailedAtLastWrite());
+
         } catch (GraphiteClient.GraphiteWriteFailed e) {
             if (logger.isDebugEnabled()) {
-                logger.debug("Failed writing to Graphite: "+e.getMessage(), e);
+                logger.debug("Failed writing to Graphite: " + e.getMessage(), e);
             } else {
-                logger.warn("Failed writing to Graphite: "+e.getMessage());
+                logger.warn("Failed writing to Graphite: " + e.getMessage());
             }
-        } catch (JolokiaClient.JolokiaClientPollingFailure e) {
-            if (logger.isDebugEnabled()) {
-                logger.debug("Failed polling metrics from Jolokia: " + e.getMessage(), e);
-            } else {
-                logger.warn("Failed polling metrics from Jolokia: " + e.getMessage());
-            }
-        }
-    }
-
-    private long getPollingWindowStartSeconds() {
-        long now = System.currentTimeMillis();
-        long pollingIntervalMs = TimeUnit.SECONDS.toMillis(pollingIntervalSeconds);
-        return TimeUnit.MILLISECONDS.toSeconds(now - (now % pollingIntervalMs));
-    }
-
-    private List<MetricValue> changeTimeTo(long newTime, List<MetricValue> metrics) {
-        return metrics.stream()
-                .map(m -> new MetricValue(m.getName(), m.getValue(), newTime))
-                .collect(Collectors.toList());
-    }
-
-    private void printToFile(List<MetricValue> metrics) {
-        for (MetricValue v : metrics) {
-            logger.trace(v.toString());
         }
     }
 
     private void sendToGraphite(List<MetricValue> metrics) {
         graphiteClient.sendMetrics(metrics);
     }
-
-    private JolokiaClient createJolokiaClient() {
-        return new JolokiaClient(jolokiaUrl);
-    }
-
-    @Override
-    public void execute(JobExecutionContext context) throws JobExecutionException {
-        poll();
-    }
-
 
     //FIXME not called for now
     public void close() {
@@ -131,6 +66,4 @@ public class MetricsPipeline implements Job {
             logger.info("Failed closing graphite client. Error = "+e.getMessage(), e);
         }
     }
-
-
 }
